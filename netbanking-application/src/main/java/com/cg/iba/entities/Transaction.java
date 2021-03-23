14368f6b3d6573b7d23399a439cd14274af913c5
26harinath <26harinath@gmail.com>
2021-03-23 10:43:04 +0530

    created pojo class

--- netbanking-application/src/main/java/com/cg/iba/entities/Transaction.java
+++ netbanking-application/src/main/java/com/cg/iba/entities/Transaction.java
@@ -13,5 +13,49 @@ public class Transaction {
     private Account bankAccount; 
     private TransactionStatus transactionStatus;
     private String transactionRemarks;
+	public long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public TransactionType getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(TransactionType transactionType) {
+		this.transactionType = transactionType;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	public Account getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(Account bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+	public TransactionStatus getTransactionStatus() {
+		return transactionStatus;
+	}
+	public void setTransactionStatus(TransactionStatus transactionStatus) {
+		this.transactionStatus = transactionStatus;
+	}
+	public String getTransactionRemarks() {
+		return transactionRemarks;
+	}
+	public void setTransactionRemarks(String transactionRemarks) {
+		this.transactionRemarks = transactionRemarks;
+	}
+    
+    
     
 }
