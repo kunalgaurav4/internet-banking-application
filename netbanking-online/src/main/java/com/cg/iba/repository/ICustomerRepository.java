917cc74df8cbf70c176fe08732577205ba695918
Kokkinti.Dileep Sai <78999974+Dileep0682@users.noreply.github.com>
2021-03-24 17:22:21 +0530

    added customer repo

--- netbanking-online/src/main/java/com/cg/iba/repository/ICustomerRepository.java
+++ netbanking-online/src/main/java/com/cg/iba/repository/ICustomerRepository.java
@@ -1,23 +1,21 @@
 package com.cg.iba.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.cg.iba.entities.Customer;
-import com.cg.iba.entities.Transaction;
-import com.cg.iba.exception.DetailsNotFoundException;
-import com.cg.iba.exception.EmptyListException;
-import com.cg.iba.exception.InvalidAccountException;
-import com.cg.iba.exception.InvalidDetailsException;
 
 
-public interface ICustomerRepository {
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Integer>{
 
-	public Customer  addCustomer(Customer customer) throws InvalidDetailsException;
-	public Customer updateCustomer(Customer customer)throws InvalidDetailsException ;
-	public boolean deleteCustomer(long customer_id) throws DetailsNotFoundException;
+		
 	
-	public Set<Customer> listAllCustomers(double minBalance) throws EmptyListException;
-	public Customer viewCustomerDetails(long account_id) throws InvalidAccountException,DetailsNotFoundException ;
-	
-	public  Customer findCustomerById(long customer_id) throws DetailsNotFoundException;
 }
