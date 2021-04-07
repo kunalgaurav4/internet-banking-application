c523842a38275c5e596379fd733831f7f2009f4b
Aman Paul <61023433+AmanPaul@users.noreply.github.com>
2021-04-07 11:10:52 +0530

    Update INomineeService.java

--- netbanking-online/src/main/java/com/cg/iba/service/INomineeService.java
+++ netbanking-online/src/main/java/com/cg/iba/service/INomineeService.java
@@ -1,20 +1,29 @@
 package com.cg.iba.service;
 
+/**
+ * 
+ * @author Aman Paul
+ * @version 1.0
+ */
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import org.springframework.stereotype.Service;
-
 import com.cg.iba.entities.Nominee;
+import com.cg.iba.exception.DetailsNotFoundException;
+import com.cg.iba.exception.EmptyListException;
+import com.cg.iba.exception.InvalidAccountException;
+import com.cg.iba.exception.InvalidDetailsException;
 
-
-@Service
 public interface INomineeService {
 	public List<Nominee> addNominee(Nominee nominee);
+
 	public List<Nominee> updateNominee(Nominee nominee);
+
 	public List<Nominee> deleteNominee(int nomineeId);
+
 	public Nominee findNomineeById(int nomineeId);
-	public List<Nominee> listAllNominees(int accountId);
+
+	public Set<Nominee> listAllNominees(int accountId);
 }
