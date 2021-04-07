cd7996e0bfdc79960a95dd2963d50ee88215105d
Aman Paul <61023433+AmanPaul@users.noreply.github.com>
2021-04-07 11:09:31 +0530

    Updated the repo

--- netbanking-online/src/main/java/com/cg/iba/repository/INomineeRepository.java
+++ netbanking-online/src/main/java/com/cg/iba/repository/INomineeRepository.java
@@ -1,22 +1,22 @@
 package com.cg.iba.repository;
 
-
 import java.util.Set;
-
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.cg.iba.entities.Account;
 import com.cg.iba.entities.Nominee;
 
-
-
+/**
+ * 
+ * @author Aman Paul
+ * @version 1.0
+ */
 @Repository
-public interface INomineeRepository extends JpaRepository<Nominee, Integer>{
-	
-	//@Query("select e from Nominee e where e.accountId='accountId'")
-	//public Set<Account> findByAccount(@Param("accountId")int accountId);
+public interface INomineeRepository extends JpaRepository<Nominee, Integer> {
+
+	@Query("select n from Nominee n where n.account.accountId=:id")
+	public Set<Nominee> findByAccount(@Param("id") long id);
 }
