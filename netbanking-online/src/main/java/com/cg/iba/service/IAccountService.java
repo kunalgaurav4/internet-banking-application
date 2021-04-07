29b8de3d8bed58dcae4e15f199206d985c59f8c6
Kunal Gaurav <49985097+kunalgaurav4@users.noreply.github.com>
2021-04-07 11:30:29 +0530

    added transaction

--- netbanking-online/src/main/java/com/cg/iba/service/IAccountService.java
+++ netbanking-online/src/main/java/com/cg/iba/service/IAccountService.java
@@ -1,27 +1,50 @@
 package com.cg.iba.service;
 
 import java.util.List;
+import java.util.Set;
 
 import com.cg.iba.entities.Account;
+import com.cg.iba.entities.Customer;
 import com.cg.iba.entities.Nominee;
 import com.cg.iba.entities.SavingsAccount;
 import com.cg.iba.entities.TermAccount;
+import com.cg.iba.entities.Transaction;
+import com.cg.iba.entities.TransactionStatus;
 
+/**
+ * @author Kunal Gaurav
+ * @version 1.0
+ */
 public interface IAccountService {
 	public List<Account> addAccount(Account saving);
-	
+
 	public List<SavingsAccount> addSavingsAccount(SavingsAccount saving);
 
 	public List<TermAccount> addTermAccount(TermAccount term);
 
-	public SavingsAccount deleteSavingId(int accountId);
-	
-	public TermAccount deleteTermId(int accountId);
+	public boolean deleteSavingId(long accountId);
 
-	public Account getAccountByID(int accountId);
+	public boolean deleteTermId(long accountId);
+
+	public Account getAccountByID(long accountId);
 
 	public List<SavingsAccount> updateSavingsAccount(SavingsAccount updS);
 
 	public List<TermAccount> updateTermAccount(TermAccount updT);
+
+	public Transaction withdraw(double amount, long accountId, long CustomerId, String password);
+
+	public List<Account> viewAccounts(long customerId);
+
+	public List<Account> viewSavingAcc(long customerId);
+
+	public List<Account> viewTermAcc(long customerId);
+
+	public Transaction deposit(double amount, long accountId);
+
+	public Transaction transferMoney(long senderAccountId, long reciverAccountId, double amount, long customerId,
+			String password);
+
+
 
 }
