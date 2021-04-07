06832bb61cf317b25f5fb70db2a791d0dec6f349
rahulkhuraijam <37807729+rahulkhuraijam@users.noreply.github.com>
2021-04-07 11:10:45 +0530

    Update IBeneficiaryService.java

--- netbanking-online/src/main/java/com/cg/iba/service/IBeneficiaryService.java
+++ netbanking-online/src/main/java/com/cg/iba/service/IBeneficiaryService.java
@@ -1,16 +1,29 @@
 package com.cg.iba.service;
 
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import com.cg.iba.entities.Beneficiary;
+import com.cg.iba.exception.DetailsNotFoundException;
+import com.cg.iba.exception.EmptyListException;
+import com.cg.iba.exception.InvalidAccountException;
+import com.cg.iba.exception.InvalidDetailsException;
 
+/**
+ * 
+ * @author Rahul Khuraijam
+ * @version 1.0
+ *
+ */
 public interface IBeneficiaryService {
 	public List<Beneficiary> addBeneficiary(Beneficiary beneficiary);
+
 	public List<Beneficiary> updateBeneficiary(Beneficiary beneficiary);
-	public Boolean  deleteBeneficiary(long beneficiaryId);
+
+	public List<Beneficiary> deleteBeneficiary(long beneficiaryId);
+
 	public Beneficiary findBeneficiaryById(long beneficiaryId);
-	//public Set<Nominee> listAllNominees(int accountid);
+
+	public Set<Beneficiary> listAllBeneficiaries(long accountid);
 }
