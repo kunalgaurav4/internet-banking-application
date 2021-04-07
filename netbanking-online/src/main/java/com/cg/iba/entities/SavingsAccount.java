54fe20cb5e3c82fef5cbeb10591140ea1479ad5e
Aman Paul <61023433+AmanPaul@users.noreply.github.com>
2021-04-07 11:26:04 +0530

    Update SavingsAccount.java

--- netbanking-online/src/main/java/com/cg/iba/entities/SavingsAccount.java
+++ netbanking-online/src/main/java/com/cg/iba/entities/SavingsAccount.java
@@ -1,31 +1,38 @@
 package com.cg.iba.entities;
 
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-
+/**
+ * 
+ * @author Kunal Gaurav
+ * @version 1.0
+ */
 @Entity
-public class SavingsAccount extends Account{
+public class SavingsAccount extends Account {
 
-    private double minBalance;
-    private double fine;
+	private double minBalance = 3000;
+	private double fine = 100;
+
+	public SavingsAccount() {
+
+	}
+
 	public double getMinBalance() {
 		return minBalance;
 	}
+
 	public void setMinBalance(double minBalance) {
 		this.minBalance = minBalance;
 	}
+
 	public double getFine() {
 		return fine;
 	}
+
 	public void setFine(double fine) {
 		this.fine = fine;
 	}
-	public SavingsAccount(double minBalance, double fine) {
-		super();
-		this.minBalance = minBalance;
-		this.fine = fine;
-	} 
 
-	public SavingsAccount() {
-		
-	}
 }
