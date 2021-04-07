ff57c0f634f9d25671f344c19a5865ed732a1bbd
Kokkinti.Dileep Sai <78999974+Dileep0682@users.noreply.github.com>
2021-04-07 11:35:48 +0530

    update ICustomerService.java

--- netbanking-online/src/main/java/com/cg/iba/service/ICustomerService.java
+++ netbanking-online/src/main/java/com/cg/iba/service/ICustomerService.java
@@ -1,23 +1,26 @@
 package com.cg.iba.service;
 
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import com.cg.iba.exception.DetailsNotFoundException;
-//import com.cg.iba.exception.EmptyListException;
-//import com.cg.iba.exception.InvalidAccountException;
 import com.cg.iba.exception.InvalidDetailsException;
-
 
 import com.cg.iba.entities.Customer;
 
+/**
+ * 
+ * @author Dileep
+ * @version 1.0
+ *
+ */
 public interface ICustomerService {
 	public List<Customer> addCustomer(Customer customer);
+
 	public List<Customer> updateCustomer(Customer customer);
-	public List<Customer> deleteCustomer(int customerId);
-	public Customer findCustomerById(int customerId);
-	//public Set<Customer> listAllCustomers(double minBalance);
-	//public Customer viewCustomerDetails(int accountid);
-	
+
+	public List<Customer> deleteCustomer(long customerId);
+
+	public Customer findCustomerById(long customerId);
+
 }
