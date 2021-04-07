038b8683b1fbe7522ce3f3df6c7f4cd85d219f22
eishatallam <79912703+eishatallam@users.noreply.github.com>
2021-04-07 11:14:24 +0530

    update serviceimpl

--- netbanking-online/src/main/java/com/cg/iba/ServiceImpl/UserServiceImpl.java
+++ netbanking-online/src/main/java/com/cg/iba/ServiceImpl/UserServiceImpl.java
@@ -3,30 +3,43 @@ package com.cg.iba.service;
 import java.util.List;
 import java.util.Optional;
 
-
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cg.iba.bean.User;
+import com.cg.iba.entities.User;
 import com.cg.iba.exception.InvalidDetailsException;
 import com.cg.iba.repository.IUserRepository;
 
+/**
+ * 
+ * @author Eisha
+ * @version 1.0
+ *
+ */
 @Service
 @Transactional
-public class UserServiceImpl implements IUserService {
+public class IUserServiceImpl implements IUserService {
 
 	@Autowired
 	private IUserRepository userRep;
 
+	/**
+	 * @param User
+	 * @return List<User>
+	 */
 	@Override
-	public List<User> addNewUser(User user){
+	public List<User> addNewUser(User user) {
 		// TODO Auto-generated method stub
 		userRep.saveAndFlush(user);
 		return userRep.findAll();
 	}
 
+	/**
+	 * @param User
+	 * @return List<User>
+	 */
 	@Override
 	public List<User> updateUserInfo(User user) {
 		// TODO Auto-generated method stub
@@ -34,38 +47,17 @@ public class UserServiceImpl implements IUserService {
 		return userRep.findAll();
 	}
 
+	/**
+	 * @param customerId
+	 * @return Optional<User>
+	 */
 	@Override
-	public Optional<User> deleteUserInfo(long userId) {
+	public Optional<User> deleteUserInfo(long customerId) {
 		// TODO Auto-generated method stub
-		Optional<User> user =userRep.findById(userId);
-		if(user!= null)
-			userRep.deleteById(userId);
+		Optional<User> user = userRep.findById(customerId);
+		if (user != null)
+			userRep.deleteById(customerId);
 		return user;
 	}
-
-	@Override
-	public User signIn(User user) throws InvalidDetailsException {
-		// TODO Auto-generated method stub
-		Optional<User> user1=userRep.findById(user.getUserId());
-		if(user1.isPresent()) {
-			return user;
-		}
-		else {
-			throw new InvalidDetailsException("Invalid userId or password");
-		}
-	}
-
-//	@Override
-//	public String signIn(Long userId, String password) {
-//		// TODO Auto-generated method stub
-//		String password1=userRep.getUserId(userId).getPassword();
-//		if(password1.equals(password)) {
-//			return "Login-Success";
-//		}
-//		else {
-//			return "Invalid userid or password";
-//		}
-//		
-//	}
 
 }
