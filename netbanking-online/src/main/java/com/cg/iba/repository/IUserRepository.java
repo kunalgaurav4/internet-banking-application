ee1c24ed2d94a6bd84a348639967fc75ce63711f
eishatallam <79912703+eishatallam@users.noreply.github.com>
2021-04-07 11:09:32 +0530

    updated repository

--- netbanking-online/src/main/java/com/cg/iba/repository/IUserRepository.java
+++ netbanking-online/src/main/java/com/cg/iba/repository/IUserRepository.java
@@ -3,13 +3,15 @@ package com.cg.iba.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cg.iba.bean.User;
+import com.cg.iba.entities.User;
 
-
+/**
+ * 
+ * @author Eisha
+ * @version 1.0
+ *
+ */
 @Repository
-public interface IUserRepository extends  JpaRepository<User,Long>{
-
+public interface IUserRepository extends JpaRepository<User, Long> {
 
 }
-
-
