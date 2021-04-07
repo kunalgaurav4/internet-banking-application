3a0a2fefe72db29da7de89a8c1cd2157e3f638f7
gayathri2599 <81209339+gayathri2599@users.noreply.github.com>
2021-04-07 11:41:20 +0530

    updated IAdminrepository.java

--- netbanking-online/src/main/java/com/cg/iba/repository/IAdminRepository.java
+++ netbanking-online/src/main/java/com/cg/iba/repository/IAdminRepository.java
@@ -1,18 +1,23 @@
 package com.cg.iba.repository;
 
-import java.util.Set;
+
 
 import com.cg.iba.entities.Admin;
 
-import com.cg.iba.exception.DetailsNotFoundException;
-import com.cg.iba.exception.EmptyListException;
-import com.cg.iba.exception.InvalidDetailsException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/**
+ * 
+ * @author Gayathri
+ * @version 1.0
+ *
+ */
+@Repository
 
-public interface IAdminRepository {
+public interface IAdminRepository  extends JpaRepository<Admin,Long>
+{
 
-	public Admin  addAdmin(Admin admin) throws InvalidDetailsException;
-	public Admin findAdminById(long adminId)throws DetailsNotFoundException ;
-	public Admin updateAdmin(Admin admin) throws InvalidDetailsException ;
-	public boolean removeAdmin(long adminId)throws DetailsNotFoundException;
-	public Set<Admin> listAllAdmins() throws EmptyListException;
+	
+
 }
+
