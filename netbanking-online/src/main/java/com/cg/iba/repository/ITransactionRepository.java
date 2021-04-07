4b3703b0e2718943be46051b4438e2428e2b31b6
26harinath <79014756+26harinath@users.noreply.github.com>
2021-04-07 11:10:38 +0530

    Update ITransactionRepository.java
    Transaction repo added

--- netbanking-online/src/main/java/com/cg/iba/repository/ITransactionRepository.java
+++ netbanking-online/src/main/java/com/cg/iba/repository/ITransactionRepository.java
@@ -1,12 +1,23 @@
 package com.cg.iba.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.cg.iba.entities.Transaction;
 
+/**
+ * 
+ * @author Harinath Reddy
+ * @version 1.0
+ *
+ */
 @Repository
-public interface ITransactionRepository extends JpaRepository<Transaction, Integer> {
+public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
 
-//    List findByAccountNumberEquals(String accountNumber);
+	@Query("select t from Transaction t where t.account.accountId=:id")
+	public List<Transaction> getAllMyAccTransactions(@Param("id") long id);
 
 }
